43a16940412a86dc1478142fb9a3a85324c04ff5
afuru <afuru@LAPTOP-5J7ML5FK>
2022-10-03 00:06:51 +0900

    プロジェクト ファイルを追加します。

---
+++ member_kanri/Form1.Designer.cs
@@ -0,0 +1,307 @@
+﻿namespace member_kanri
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.name_box = new System.Windows.Forms.TextBox();
+            this.affiliation_box = new System.Windows.Forms.TextBox();
+            this.sex_box = new System.Windows.Forms.TextBox();
+            this.age_box = new System.Windows.Forms.TextBox();
+            this.comment_box = new System.Windows.Forms.TextBox();
+            this.close_button = new System.Windows.Forms.Button();
+            this.addition_button = new System.Windows.Forms.Button();
+            this.delete_button = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.output_button = new System.Windows.Forms.Button();
+            this.edit_button = new System.Windows.Forms.Button();
+            this.lable = new System.Windows.Forms.Label();
+            this.id_box = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("BIZ UDPゴシック", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label2.Location = new System.Drawing.Point(130, 272);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 23);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "所属";
+            // 
+            // label3
+            // 
+            this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("BIZ UDPゴシック", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label3.Location = new System.Drawing.Point(130, 217);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(56, 23);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "年齢";
+            // 
+            // label4
+            // 
+            this.label4.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("BIZ UDPゴシック", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label4.Location = new System.Drawing.Point(130, 159);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(56, 23);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "性別";
+            // 
+            // label5
+            // 
+            this.label5.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("BIZ UDPゴシック", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label5.Location = new System.Drawing.Point(102, 324);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(89, 23);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "コメント";
+            // 
+            // name_box
+            // 
+            this.name_box.Font = new System.Drawing.Font("BIZ UDPゴシック", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.name_box.Location = new System.Drawing.Point(218, 97);
+            this.name_box.Margin = new System.Windows.Forms.Padding(5);
+            this.name_box.Multiline = true;
+            this.name_box.Name = "name_box";
+            this.name_box.Size = new System.Drawing.Size(395, 43);
+            this.name_box.TabIndex = 1;
+            // 
+            // affiliation_box
+            // 
+            this.affiliation_box.Font = new System.Drawing.Font("BIZ UDPゴシック", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.affiliation_box.Location = new System.Drawing.Point(218, 265);
+            this.affiliation_box.Margin = new System.Windows.Forms.Padding(4);
+            this.affiliation_box.Multiline = true;
+            this.affiliation_box.Name = "affiliation_box";
+            this.affiliation_box.Size = new System.Drawing.Size(395, 43);
+            this.affiliation_box.TabIndex = 4;
+            // 
+            // sex_box
+            // 
+            this.sex_box.Font = new System.Drawing.Font("BIZ UDPゴシック", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.sex_box.Location = new System.Drawing.Point(218, 152);
+            this.sex_box.Margin = new System.Windows.Forms.Padding(4);
+            this.sex_box.Multiline = true;
+            this.sex_box.Name = "sex_box";
+            this.sex_box.Size = new System.Drawing.Size(395, 43);
+            this.sex_box.TabIndex = 2;
+            // 
+            // age_box
+            // 
+            this.age_box.Font = new System.Drawing.Font("BIZ UDPゴシック", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.age_box.Location = new System.Drawing.Point(218, 210);
+            this.age_box.Margin = new System.Windows.Forms.Padding(4);
+            this.age_box.Multiline = true;
+            this.age_box.Name = "age_box";
+            this.age_box.Size = new System.Drawing.Size(395, 43);
+            this.age_box.TabIndex = 3;
+            // 
+            // comment_box
+            // 
+            this.comment_box.Font = new System.Drawing.Font("BIZ UDPゴシック", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.comment_box.Location = new System.Drawing.Point(218, 324);
+            this.comment_box.Margin = new System.Windows.Forms.Padding(4);
+            this.comment_box.Multiline = true;
+            this.comment_box.Name = "comment_box";
+            this.comment_box.Size = new System.Drawing.Size(392, 198);
+            this.comment_box.TabIndex = 5;
+            // 
+            // close_button
+            // 
+            this.close_button.Font = new System.Drawing.Font("BIZ UDPゴシック", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.close_button.Location = new System.Drawing.Point(619, 642);
+            this.close_button.Margin = new System.Windows.Forms.Padding(4);
+            this.close_button.Name = "close_button";
+            this.close_button.Size = new System.Drawing.Size(118, 39);
+            this.close_button.TabIndex = 11;
+            this.close_button.Text = "とじる";
+            this.close_button.UseVisualStyleBackColor = true;
+            // 
+            // addition_button
+            // 
+            this.addition_button.Font = new System.Drawing.Font("BIZ UDPゴシック", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.addition_button.Location = new System.Drawing.Point(43, 642);
+            this.addition_button.Margin = new System.Windows.Forms.Padding(4);
+            this.addition_button.Name = "addition_button";
+            this.addition_button.Size = new System.Drawing.Size(118, 39);
+            this.addition_button.TabIndex = 7;
+            this.addition_button.Text = "追加";
+            this.addition_button.UseVisualStyleBackColor = true;
+            this.addition_button.Click += new System.EventHandler(this.addition_button_Click);
+            // 
+            // delete_button
+            // 
+            this.delete_button.Font = new System.Drawing.Font("BIZ UDPゴシック", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.delete_button.Location = new System.Drawing.Point(417, 642);
+            this.delete_button.Margin = new System.Windows.Forms.Padding(4);
+            this.delete_button.Name = "delete_button";
+            this.delete_button.Size = new System.Drawing.Size(118, 39);
+            this.delete_button.TabIndex = 10;
+            this.delete_button.Text = "削除";
+            this.delete_button.UseVisualStyleBackColor = true;
+            this.delete_button.Click += new System.EventHandler(this.delete_button_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 20;
+            this.listBox1.Location = new System.Drawing.Point(218, 530);
+            this.listBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(395, 104);
+            this.listBox1.TabIndex = 6;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged_1);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("BIZ UDPゴシック", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label1.Location = new System.Drawing.Point(130, 104);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(56, 23);
+            this.label1.TabIndex = 14;
+            this.label1.Text = "名前";
+            // 
+            // output_button
+            // 
+            this.output_button.Font = new System.Drawing.Font("BIZ UDPゴシック", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.output_button.Location = new System.Drawing.Point(292, 642);
+            this.output_button.Name = "output_button";
+            this.output_button.Size = new System.Drawing.Size(118, 39);
+            this.output_button.TabIndex = 9;
+            this.output_button.Text = "出力";
+            this.output_button.UseVisualStyleBackColor = true;
+            this.output_button.Visible = false;
+            // 
+            // edit_button
+            // 
+            this.edit_button.Font = new System.Drawing.Font("BIZ UDPゴシック", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.edit_button.Location = new System.Drawing.Point(168, 642);
+            this.edit_button.Name = "edit_button";
+            this.edit_button.Size = new System.Drawing.Size(118, 39);
+            this.edit_button.TabIndex = 8;
+            this.edit_button.Text = "編集";
+            this.edit_button.UseVisualStyleBackColor = true;
+            this.edit_button.Visible = false;
+            // 
+            // lable
+            // 
+            this.lable.AutoSize = true;
+            this.lable.Font = new System.Drawing.Font("BIZ UDPゴシック", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.lable.Location = new System.Drawing.Point(106, 50);
+            this.lable.Name = "lable";
+            this.lable.Size = new System.Drawing.Size(85, 23);
+            this.lable.TabIndex = 17;
+            this.lable.Text = "社員ID";
+            // 
+            // id_box
+            // 
+            this.id_box.Font = new System.Drawing.Font("BIZ UDPゴシック", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.id_box.Location = new System.Drawing.Point(218, 43);
+            this.id_box.Multiline = true;
+            this.id_box.Name = "id_box";
+            this.id_box.Size = new System.Drawing.Size(395, 43);
+            this.id_box.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(785, 704);
+            this.Controls.Add(this.id_box);
+            this.Controls.Add(this.lable);
+            this.Controls.Add(this.edit_button);
+            this.Controls.Add(this.output_button);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.delete_button);
+            this.Controls.Add(this.addition_button);
+            this.Controls.Add(this.close_button);
+            this.Controls.Add(this.comment_box);
+            this.Controls.Add(this.age_box);
+            this.Controls.Add(this.sex_box);
+            this.Controls.Add(this.affiliation_box);
+            this.Controls.Add(this.name_box);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Font = new System.Drawing.Font("ＭＳ ゴシック", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox name_box;
+        private System.Windows.Forms.TextBox affiliation_box;
+        private System.Windows.Forms.TextBox sex_box;
+        private System.Windows.Forms.TextBox age_box;
+        private System.Windows.Forms.TextBox comment_box;
+        private System.Windows.Forms.Button close_button;
+        private System.Windows.Forms.Button addition_button;
+        private System.Windows.Forms.Button delete_button;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button output_button;
+        private System.Windows.Forms.Button edit_button;
+        private System.Windows.Forms.Label lable;
+        private System.Windows.Forms.TextBox id_box;
+    }
+}
+
---
+++ member_kanri/Form1.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace member_kanri
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //追加
+        private void addition_button_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < listBox1.Items.Count; i++)
+            {
+                //リストボックスの中の行の集まりの中の［i］行目(1人分) 
+                //listBox1.Items[i];をhogeという変数に代入してる。ifで省略して書ける
+                //object hoge = listBox1.Items[i];
+
+                //idboxにはいってる値と同じ値をもつ行があるとき（リストボックスの中のiの情報のなかの[0]）
+                if (id_box.Text == listBox1.Items[i].ToString().Split(' ')[0])
+                {
+                    //エラーメッセージを表示して処理を終了
+                    MessageBox.Show("入力できません",
+                        "エラー",
+                        MessageBoxButtons.OK,
+                        MessageBoxIcon.Error);
+                    return;
+                }
+            }
+            listBox1.Items.Add(id_box.Text + " " + name_box.Text + " " + sex_box.Text + " " + age_box.Text + " " + affiliation_box.Text + " " + comment_box.Text);
+        }
+        //削除
+        private void delete_button_Click(object sender, EventArgs e)
+        {
+            //もし選択されたら、選択されたものを消す
+            if (listBox1.SelectedIndex != -1)
+            {
+                listBox1.Items.Remove(listBox1.SelectedItem);
+            }
+            
+            //選択されなかったらメッセージボックスを出す
+            else
+            {
+                MessageBox.Show("選択してください",
+                 "エラー",
+                MessageBoxButtons.OK,
+                MessageBoxIcon.Error);
+            }        
+        }
+        //リストボックス
+        private void listBox1_SelectedIndexChanged_1(object sender, EventArgs e)
+        {
+            ListBox listBox = sender as ListBox;
+            //listbox.textをtextにした
+            string text = listBox.Text;
+            //↑のtextを.spritで配列にする
+            string[] splitText = text.Split(' ');
+            id_box.Text = splitText[0];
+            name_box.Text = splitText[1];
+            sex_box.Text = splitText[2];
+            age_box.Text = splitText[3];
+            affiliation_box.Text = splitText[4];
+            comment_box.Text = splitText[5];
+        }
+
+        private void textBox5_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+      
+    }
+}
